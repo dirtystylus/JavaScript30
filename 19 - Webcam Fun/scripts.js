055e7c7d80ca8cd167f4cf2f692a80263284a218
Mark Llobrera <mark@dirtystylus.com>
2018-05-07 13:05:49 -0400

    rgb split

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,7 +24,9 @@ function paintToCanvas() {
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
         let pixels = ctx.getImageData(0, 0, width, height);
-        pixels = redEffect(pixels);
+        // pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
+        ctx.globalAlpha = 0.8;
         ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
@@ -34,6 +36,15 @@ function redEffect(pixels) {
         pixels.data[i + 0] = pixels.data[i + 0] + 100;
         pixels.data[i + 1] = pixels.data[i + 1] - 50;
         pixels.data[i + 2] = pixels.data[i + 2] * .5;
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0];
+        pixels.data[i + 500] = pixels.data[i + 1];
+        pixels.data[i - 550] = pixels.data[i + 2];
     }
     return pixels;
 }
